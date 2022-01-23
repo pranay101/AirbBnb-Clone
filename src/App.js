3964d5930f396169bb82ae31b78ff1df2847c03d
pranay101 <pranayprajapati101@gmail.com>
2022-01-23 19:09:06 +0530

    Implemented footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
+import Footer from "./Components/Footer/Footer"
 
 function App() {
   return (
     <div className="App">
      <Header />
      <Home />
+     <Footer />
     </div>
   );
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,8 @@ import classes from "./Footer.module.css"
 
 function Footer() {
   return <div className={classes.footer}>
-
+<p>2022 Airbnb Clone! no rights reserved this is a demo?</p>
+  <p>Privacy Terms Sitemap Company Details</p>
   </div>;
 }
 
